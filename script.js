21f5faffb34531ff9c2f467e95ee57d15a481672
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-05-12 21:05:59 +0530

    adding books to library array is build. library display in progress

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const addBook = document.querySelector('#addBook');
-
 const dialog = document.querySelector('dialog');
-
 const cancel = document.querySelector('#cancel');
+const conf = document.querySelector('#confirm');
+const form = document.querySelector('form');
+const libraryDisplay = document.querySelector('#lib');
 
 addBook.addEventListener('click',()=>{
     console.log('hi');
@@ -13,3 +14,36 @@ cancel.addEventListener('click',(e)=>{
     e.preventDefault();
     dialog.close();
 })
+
+function bookConstructor(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+bookConstructor.prototype.toggleRead = function(){
+    this.read=!this.read;    
+}
+
+let myLib = []
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const book = new bookConstructor(
+        data.get('title'),
+        data.get('author'),
+        data.get('pages'),
+        data.get('read') === 'on'
+    );
+    e.target.reset();
+    myLib.push(book);
+    console.log(myLib);
+    dialog.close();
+})
+
+function display(){
+    
+}
